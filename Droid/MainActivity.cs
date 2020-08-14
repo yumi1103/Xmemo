ef91706c51c49625b2e1c2570c94c6e377f99d50
yumi1103 <tanisi.1103@gmail.com>
2020-08-14 21:31:01 +0900

    * .gitignore: プロジェクト作成 AndroidのUI・イベントハンドラ作成
    * Xmemo.sln:
    * Main.cs:
    * Info.plist:
    * AppDelegate.cs:
    * Xmemo.csproj:
    * Main.storyboard:
    * MemoHolder.cs:
    * Xmemo.iOS.csproj:
    * ViewController.cs:
    * MainActivity.cs:
    * Entitlements.plist:
    * Xmemo.Droid.csproj:
    * LaunchScreen.storyboard:
    * AboutAssets.txt:
    * ViewController.designer.cs:
    * AssemblyInfo.cs:
    * Main.axml:
    * AssemblyInfo.cs:
    * Contents.json:
    * AboutResources.txt:
    * Strings.xml:
    * Icon.png:
    * Resource.designer.cs:
    * AndroidManifest.xml:
    * Icon.png:
    * Icon.png:
    * Icon.png:
    * Icon.png:
    * Contents.json:

---
+++ Droid/MainActivity.cs
@@ -0,0 +1,56 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+
+namespace Xmemo.Droid
+{
+    [Activity(Label = "Xmemo", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+
+
+            // メモの初期値的なやつ インスタンスではなくシンタックスシュガー
+            MemoHolder.Current.Memo = new Memo()
+            {
+                Date = DateTime.Now,
+                Subject = "",
+                Text = "",
+            };
+
+            DisplayMemo();
+
+
+            //件名の修正
+            var subjectText = FindViewById<EditText>(Resource.Id.SubjectText);
+            subjectText.TextChanged += (s, e) =>
+            {
+                MemoHolder.Current.Memo.Subject = subjectText.Text;
+            };
+
+            //内容の修正
+            var memoText = FindViewById<EditText>(Resource.Id.MemoText);
+            memoText.TextChanged += (s, e) =>
+            {
+                MemoHolder.Current.Memo.Text = memoText.Text;
+            };
+
+
+        }
+
+        // メモの描画
+        private void DisplayMemo()
+        {
+            var memo = MemoHolder.Current.Memo;
+            FindViewById<EditText>(Resource.Id.DateText).Text = string.
+                Format("{0:yyyy/MMM/dd}", memo.Date);
+            FindViewById<EditText>(Resource.Id.SubjectText).Text = memo.Subject;
+            FindViewById<EditText>(Resource.Id.MemoText).Text = memo.Text;
+        }
+    }
+}
+
---
+++ Xmemo/MemoHolder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Xmemo
+{
+    public class MemoHolder
+    {
+        public static MemoHolder Current { get; } = new MemoHolder();
+        public Memo Memo { get; set; }
+
+        public MemoHolder()
+        {
+
+        }
+    }
+}
